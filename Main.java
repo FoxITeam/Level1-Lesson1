9fa28d1e068bfb426f1a038c6868e4897faeb6cc
GrayFoxIT <37319212+GrayFoxIT@users.noreply.github.com>
2018-03-13 07:01:39 +0300

    Commit 1

---
+++ Main.java
@@ -0,0 +1,13 @@
+package ru.foxit.grayfox;
+
+import static ru.foxit.grayfox.Comparison.fff;
+
+public class Main {
+    // CTRL + ALT + L - row format.
+    // psvm - public static void
+    // inside "psvm" - sout - System.out
+    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+    public static void main(String args []) {
+
+    }
+}
